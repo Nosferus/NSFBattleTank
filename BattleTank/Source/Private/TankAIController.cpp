014087b1da4981dd9bc7ea4b81776faffd80d0d1
Nosfer <nosferus83@gmail.com>
2018-01-07 17:49:54 +0300

    BT12 (118) AI Controller basics

---
+++ BattleTank/Source/Private/TankAIController.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+		UE_LOG(LogTemp, Error, TEXT("AIController Not Possessing a Tank"))
+	else
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing %s"), *(ControlledTank->GetName()));
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+
+}
--- BattleTank/Source/Private/TankPlayerController.cpp
+++ BattleTank/Source/Private/TankPlayerController.cpp
@@ -4,6 +4,17 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+		UE_LOG(LogTemp, Error, TEXT("PlayerController Not Possessing a Tank"))
+	else 
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *(ControlledTank->GetName()));
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
