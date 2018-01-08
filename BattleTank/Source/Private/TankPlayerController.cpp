72e21be11d6bedfd2363e208dc05c36f0aa198df
Nosfer <nosferus83@gmail.com>
2018-01-08 18:19:04 +0300

    BT13 (123) Targeting system In-Work

--- BattleTank/Source/Private/TankPlayerController.cpp
+++ BattleTank/Source/Private/TankPlayerController.cpp
@@ -3,14 +3,22 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+// Вызывается каждый кадр
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//UE_LOG(LogTemp, Warning, TEXT("TimeElapsed %i"), DeltaTime)
+	AimTowardsCrosshair();
+}
 
+// Вызывается один раз в начале игры
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
 	auto ControlledTank = GetControlledTank();
 	if (!ControlledTank)
-		UE_LOG(LogTemp, Error, TEXT("PlayerController Not Possessing a Tank"))
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not Possessing a Tank"))
 	else 
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *(ControlledTank->GetName()));
 }
@@ -18,5 +26,38 @@ void ATankPlayerController::BeginPlay()
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
-	
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+		return;
+
+	FVector HitLocation; //Out param
+	GetSightRayHitLocation(HitLocation);
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	//Находим экранные координаты прицела в пикселях и помещаем в ScreenLocation.
+	//Информация о положении курсора в долях единицы содержится в переменных CrosshairXLocation и CrosshairYLocation
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+		UE_LOG(LogTemp, Warning, TEXT("WorldDirection: %s"), *LookDirection.ToString());
+
+	//Выполняем депроекцию положения курсора
+//	FVector WorldDirection, CamWorldLocation;
+//	if (DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CamWorldLocation, WorldDirection))
+//		UE_LOG(LogTemp, Warning, TEXT("WorldDirection: %s"), *WorldDirection.ToString())
+	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CamWorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CamWorldLocation, LookDirection);
 }
--- BattleTank/Source/Public/TankPlayerController.h
+++ BattleTank/Source/Public/TankPlayerController.h
@@ -14,8 +14,22 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3f;
+	//FVector2D ScreenLocation;
+
 	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void AimTowardsCrosshair();
+
+	//Return an out parameter, true if hit lanscape
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
 };
