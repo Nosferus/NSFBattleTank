0a2c9bb576829020749745cea956675b9e1826a9
Nosfer <nosferus83@gmail.com>
2018-02-04 00:39:57 +0300

    BT41 (189) HealthBar Ready

--- BattleTank/Source/Public/Tank.h
+++ BattleTank/Source/Public/Tank.h
@@ -14,8 +14,10 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+
+	UFUNCTION(BlueprintPure, Category = "Info")
+	float GetHealthPercent() const { return ((float)CurrentHealth / (float)MaxHealth); }
 
 protected:
 
